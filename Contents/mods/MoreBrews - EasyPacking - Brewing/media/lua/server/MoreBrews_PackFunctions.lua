09e2516c3387d32684b2ea5adf1ea959e29803da
Maria Alexis Sales <mariaalexissales@gmail.com>
2025-03-04 23:31:34 -0600

    added necessary functions to test fullness

--- Contents/mods/MoreBrews - EasyPacking - Brewing/media/lua/server/MoreBrews_PackFunctions.lua
+++ Contents/mods/MoreBrews - EasyPacking - Brewing/media/lua/server/MoreBrews_PackFunctions.lua
@@ -27,6 +27,14 @@ function Recipe.OnTest.IsFavorite(items, result)
 	return not items:isFavorite()
 end
 
-function Recipe.OnTest.Full(items)
-	return items:getUsedDelta() == 1
+function Recipe.OnTest.WholeFood(item)
+    local baseHunger = math.abs(item:getBaseHunger())
+    local hungerChange = math.abs(item:getHungerChange())
+	if item:isFresh() then return not (baseHunger * 0.99 > hungerChange) end
+    return not ((baseHunger * 0.99) > hungerChange)
+end
+
+function Recipe.OnTest.WholeItem(item)
+	local maxItemCondition = item:getConditionMax()
+	return item:getCondition() == maxItemCondition
 end
