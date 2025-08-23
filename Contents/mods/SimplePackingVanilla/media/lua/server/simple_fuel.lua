adca14d50fddfd37eab58dd366c6e7aaa2cd7afb
Maria Alexis Sales <mariaalexissales@gmail.com>
2025-08-23 11:10:07 -0500

    haha 😢

--- Contents/mods/SimplePackingVanilla/media/lua/server/simple_fuel.lua
+++ Contents/mods/SimplePackingVanilla/media/lua/server/simple_fuel.lua
@@ -1,3 +1,14 @@
-campingFuelType["500pkRag"] = 800/60.0;
-campingFuelType["500pkLeather"] = 800/60.0;
-campingFuelType["500pkDenim"] = 800/60.0;
+campingFuelType["10pkRag"] = 50 / 60.0;
+campingFuelType["50pkRag"] = 5;
+campingFuelType["100pkRag"] = 650 / 60.0;
+campingFuelType["500pkRag"] = 800 / 60.0;
+
+campingFuelType["10pkLeather"] = 50 / 60.0;
+campingFuelType["50pkLeather"] = 5;
+campingFuelType["100pkLeather"] = 650 / 60.0;
+campingFuelType["500pkLeather"] = 800 / 60.0;
+
+campingFuelType["10pkDenim"] = 50 / 60.0;
+campingFuelType["50pkDenim"] = 5;
+campingFuelType["100pkDenim"] = 650 / 60.0;
+campingFuelType["500pkDenim"] = 800 / 60.0;
